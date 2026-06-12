97003fa9a397dc9759ecf3b327ee9f4c774746ce
AmanDwivwdicc <dwivediaman104@gmail.com>
2026-06-12 08:49:53 +0530

    learnt custom react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
 
 import Chai from './chai'
 function App() {
-
+  const username = 'chai aur code'
 
   return (
     <>
     <Chai/>
+    <h1>hello {username}</h1>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+//element format in react : 
+
+const reactElement = React.createElement(
+  'a',
+  {href : 'https://www.google.com', target : '_blank'},
+  'click me to visit google'
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <App/>
 )
